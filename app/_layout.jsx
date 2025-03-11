5ad1225cdc288cab5178217f20cb17d7e1d1a6b5
saikatadhikary.bca <saikatadhikary.bca@gmail.com>
2025-03-11 13:56:05 +0000

    updated

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,30 @@
-import { Stack } from "expo-router";
+import { Slot } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>My Expo App</Text>
+      <Slot />
+      <Text style={styles.footer}>© 2023</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    backgroundColor:"red",
+    color:"white",
+    textAlign:"center"
+  },
+  footer: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+});
--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,109 @@
-import { Text, View ,StyleSheet} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, Image, StyleSheet, FlatList, ActivityIndicator } from "react-native";
 
-export default function Index() {
+const Index = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getCoins = async () => {
+    try {
+      const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      getCoins();
+    }, 10000);
+  }, []);
+
+
+
+  const renderItem = ({ item }) => (
+    <View style={styles.coinContainer}>
+      <View style={styles.coinNameContainer}>
+        <Image style={styles.coinImage} source={{ uri: item.image }} />
+        <View>
+          <Text style={styles.coinName}>{item.name}</Text>
+          <Text style={styles.coinSymbol}>
+            {item.symbol.toUpperCase()}</Text>
+        </View>
+      </View>
+      <View style={styles.priceCounter}>
+        <Text style={styles.coinPrice}>${item.current_price}</Text>
+        <Text style={styles.coinPrice}>{item.price_change_percentage_24h}%</Text>
+      </View>
+    </View>
+  );
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={style.container}>Hello world</Text>
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }) => id}
+          renderItem={renderItem}
+        />
+      )}
     </View>
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    color: "#fff",
+    padding: 20,
+    backgroundColor: 'white'
   },
+  coinContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15
+  },
+  coinNameContainer: {
+    flexDirection: 'row',
+    alignItems: 'start',
+    gap: 10
+  },
+  coinSymbol: {
+    color: 'gray',
+    fontWeight: 'bold',
+    marginRight: 10,
+    fontSize: 10
+  },
+  coinName: {
+    color: 'black',
+    fontSize: 16,
+  },
+  coinPrice: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  coinImage: {
+    height: 35,
+    width: 35,
+    padding: 20
+  },
+  priceCounter: {
+    color: '#333',
+    fontWeight: '600',
+    fontSize: 18,
+    flexDirection: 'column',
+    alignItems: 'end',
+    justifyContent: 'end',
+    gap: 12,
+    padding: '16px 20px'
+  }
 });
+
+export default Index;
