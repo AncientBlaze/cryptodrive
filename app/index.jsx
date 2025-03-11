956f2ded627b3f35c062e5723db495cb7702f45f
saikatadhikary.bca <saikatadhikary.bca@gmail.com>
2025-03-11 08:19:01 +0000

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import { Text, View ,StyleSheet} from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={style.container}>Hello world</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+  },
+});
